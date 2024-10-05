0c8795bc272710baee2abb178f262eec5fcc1daf
Mohammad Al Faied <al15-4191@diu.edu.bd>
2024-10-05 13:40:57 +0600

    added

--- 04.py
+++ 04.py
@@ -1,2 +1,10 @@
 # Get user input for the hour of the day
 a = int(input("Enter Hour of the Day: "))
+
+# Determine and print the appropriate greeting
+if a < 10:
+    print("Good morning")
+elif a < 20:
+    print("Good day")
+else:
+    print("Good evening")
