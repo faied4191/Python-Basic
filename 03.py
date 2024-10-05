0428a37f35fc97d49a115865b586a3b41e9c7939
Mohammad Al Faied <al15-4191@diu.edu.bd>
2024-10-05 13:39:30 +0600

    added

--- 03.py
+++ 03.py
@@ -1,3 +1,9 @@
 # Get user input for two numbers
 a = float(input("Enter First Number: "))
 b = float(input("Enter Second Number: "))
+
+# Calculate the sum
+sum = a + b
+
+# Print the result
+print("The Sum is:", sum)
