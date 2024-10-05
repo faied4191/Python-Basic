4abcca8547fdddc712a2c5a7569854b0b6d19594
Mohammad Al Faied <al15-4191@diu.edu.bd>
2024-10-05 13:38:00 +0600

    added

---
+++ 02.py
@@ -0,0 +1,8 @@
+# Get user input for name and ID
+a = input("Enter Your Name: ")
+b = input("Enter Your ID: ")
+
+# Print the collected information
+print("Name:", a)
+print("ID:", b)
+print("Length of Name:", len(a))
