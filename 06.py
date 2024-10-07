f7067878df801e01e00b17b8e4ad773f9cce82c9
Mohammad Al Faied <al15-4191@diu.edu.bd>
2024-10-07 10:57:10 +0600

    added

--- 06.py
+++ 06.py
@@ -1,2 +1,20 @@
 # Get user input for the day number
 a = int(input("Enter Day Number of the Week: "))
+
+# Determine the day of the week
+if a == 1:
+    print("Monday")
+elif a == 2:
+    print("Tuesday")
+elif a == 3:
+    print("Wednesday")
+elif a == 4:
+    print("Thursday")
+elif a == 5:
+    print("Friday")
+elif a == 6:
+    print("Saturday")
+elif a == 7:
+    print("Sunday")
+else:
+    print("Invalid day number")
