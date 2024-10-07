b96244b2bd33c368e3d50749dfde6dcf0b293197
Mohammad Al Faied <al15-4191@diu.edu.bd>
2024-10-07 10:55:45 +0600

    added

--- 05.py
+++ 05.py
@@ -2,3 +2,14 @@
 a = int(input("Enter First Number: "))
 b = int(input("Enter Second Number: "))
 c = int(input("Enter Third Number: "))
+
+# Determine the greatest number
+if a > b and a > c:
+    greatest = a
+elif b > a and b > c:
+    greatest = b
+else:
+    greatest = c
+
+# Print the greatest number
+print("The Greatest is:", greatest)
