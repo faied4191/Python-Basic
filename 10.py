7b3360148ed7a55d212fc6b9a57efc184afe3371
Mohammad Al Faied <al15-4191@diu.edu.bd>
2024-10-09 05:23:37 +0600

    added

--- 10.py
+++ 10.py
@@ -1,3 +1,10 @@
 # Define the function to calculate the sum
 def is_sum(x, y):
     return x + y
+
+# Define the function to find the maximum of two numbers
+def is_max(x, y):
+    if x > y:
+        return x
+    else:
+        return y
