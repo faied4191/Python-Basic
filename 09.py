3ab383af0f88fe07f49c9bbcb4e6f82b0704a4aa
Mohammad Al Faied <al15-4191@diu.edu.bd>
2024-10-09 05:21:57 +0600

    added

--- 09.py
+++ 09.py
@@ -1,3 +1,7 @@
 # Define the function to calculate the sum
 def is_sum(x, y):
     return x + y
+
+# Get user input for two numbers
+a = float(input("Enter the first number: "))
+b = float(input("Enter the second number: "))
