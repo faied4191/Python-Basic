f9588338951e53bc7ab48054741ca53fea49c6be
Mohammad Al Faied <al15-4191@diu.edu.bd>
2024-10-14 23:37:13 +0600

    added

--- 11.py
+++ 11.py
@@ -1,2 +1,12 @@
 # Initialize the list with car brands
 arr1 = ["Volvo", "BMW", "Ford", "Mazda"]
+
+# Modify the first element
+arr1[0] = "Opel"
+
+# Print the first element and its length
+print("First Element Name:", arr1[0])
+print("First Element Size:", len(arr1[0]))
+
+# Print the size of the array
+print("Array Size:", len(arr1))
