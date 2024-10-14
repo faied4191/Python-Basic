9368f5bc73a3f68921a1d1380fc9016347a95e49
Mohammad Al Faied <al15-4191@diu.edu.bd>
2024-10-15 00:03:16 +0600

    added

--- 14.py
+++ 14.py
@@ -3,3 +3,8 @@ class Book:
         self.title = title
         self.author = author
         self.year = year
+
+    def display(self):
+        print("Title:", self.title)
+        print("Author:", self.author)
+        print("Year:", self.year)
