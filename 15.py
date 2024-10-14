e86a6f71851c3abfbba6f54d56ca81ea742f3def
Mohammad Al Faied <al15-4191@diu.edu.bd>
2024-10-15 00:05:19 +0600

    added

--- 15.py
+++ 15.py
@@ -2,3 +2,7 @@ class Rectangle:
     def __init__(self, length, width):
         self.length = length
         self.width = width
+
+    def area(self):
+        area = self.length * self.width
+        print("Area:", area)
