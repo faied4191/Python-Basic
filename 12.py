a5fac1a4a6583799c722223bb66b498c81333031
Mohammad Al Faied <al15-4191@diu.edu.bd>
2024-10-14 23:39:10 +0600

    added

--- 12.py
+++ 12.py
@@ -3,3 +3,15 @@ a = int(input("Enter the number of elements: "))
 
 # Initialize an empty list
 arr1 = []
+
+# Populate the list with user input
+for i in range(a):
+    element = input(f"Enter element name {i + 1}: ")
+    arr1.append(element)
+
+# Print the first element and its length
+print("First Element Name:", arr1[0])
+print("First Element Size:", len(arr1[0]))
+
+# Print the size of the list
+print("Array Size:", len(arr1))
